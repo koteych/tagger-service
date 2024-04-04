785b177997d96c8de54c50a7d81262f49cb8dec2
koteych <154607050+koteych@users.noreply.github.com>
2024-04-04 19:13:10 +0300

    Remove unnecessary route

--- api/src/main.ts
+++ api/src/main.ts
@@ -16,10 +16,6 @@ dataSource
     console.error("Error during Data Source initialization:", err)
   })
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Express with TypeScript!');
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
