90348cf90dfddbffa1706b19eb6068b6234c800e
koteych <154607050+koteych@users.noreply.github.com>
2024-04-04 21:46:08 +0300

    Add logger middleware

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from 'express';
+import morgan from 'morgan';
 import { dataSource } from './config/db'
 import tagsRouter from './controllers/tags'
 import picturesRouter from './controllers/pictures'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const loggerMiddleware = morgan('dev');
+
+app.use(loggerMiddleware);
 
 app.use('/tags', tagsRouter);
 app.use('/pictures', picturesRouter);
