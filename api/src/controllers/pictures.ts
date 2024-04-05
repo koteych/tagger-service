c923ff48545eb6e12150f7e4ea42644062f00e4d
koteych <154607050+koteych@users.noreply.github.com>
2024-04-05 16:57:17 +0300

    Add remove-tag and add-tag endpoints for pictures

--- api/src/controllers/pictures.ts
+++ api/src/controllers/pictures.ts
@@ -3,14 +3,34 @@ import { TagService } from '../services/tags';
 import { PictureService } from '../services/pictures';
 import { dataSource } from '../config/db';
 import { Picture } from '../entities/picture';
+import {Tag} from '../entities/tag';
 
 const router = express.Router();
 
 const pictureRepository = dataSource.getRepository(Picture);
-const pictureService = new PictureService(pictureRepository);
+const tagRepository = dataSource.getRepository(Tag);
+
+const pictureService = new PictureService(pictureRepository, tagRepository);
 
 router.get('/', async (req: Request, res: Response) => {
     return res.send(await pictureService.getAll());
 })
+
+router.post('/:id/add-tag/:tid', async (req: Request, res: Response) => {
+    const pictureId = Number(req.params.id);
+    const tagId     = Number(req.params.tid);
+
+    pictureService.addTag(pictureId, tagId);
+    return res.send({ok: 200});
+})
+
+router.post('/:id/remove-tag/:tid', async (req: Request, res: Response) => {
+    const pictureId = Number(req.params.id);
+    const tagId     = Number(req.params.tid);
+
+    pictureService.removeTag(pictureId, tagId);
+    return res.send({ok: 200});;
+})
+
 
 export default router;
--- api/src/services/pictures.ts
+++ api/src/services/pictures.ts
@@ -3,8 +3,12 @@ import { Tag } from '../entities/tag';
 import { Picture } from '../entities/picture';
 
 export class PictureService {
-    constructor(private readonly pictureRepository: Repository<Picture>) {
+    constructor(
+        private readonly pictureRepository: Repository<Picture>,
+        private readonly tagRepository: Repository<Tag>
+    ) {
         this.pictureRepository = pictureRepository;
+        this.tagRepository = tagRepository;
     }
 
     async getAll(): Promise<Picture[]> {
@@ -21,5 +25,42 @@ export class PictureService {
         picture.title = 'some picture';
 
         return this.pictureRepository.save(picture);
+    }
+
+    async addTag(pictureId: number, tagId: number): Promise<void> {
+        const tag = await this.tagRepository.findOne({
+            where: {
+                id: tagId
+            }
+        });
+
+        if (!tag) return;
+
+        const picture = await this.pictureRepository.findOne({
+            where: {
+                id: pictureId
+            }, relations: {
+                tags: true
+            }
+        });
+
+        // if tag already added
+        if (picture.tags.some((t) => t.id === tag.id)) return;
+
+        picture.tags.push(tag);
+        this.pictureRepository.save(picture);
+    }
+
+    async removeTag(pictureId: number, tagId: number): Promise<void> {
+        const picture = await this.pictureRepository.findOne({
+            where: {
+                id: pictureId
+            }, relations: {
+                tags: true
+            }
+        });
+
+        picture.tags = picture.tags.filter(t => t.id !== tagId);
+        this.pictureRepository.save(picture);
     }
 }
